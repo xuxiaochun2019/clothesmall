4d4f1fe018c45b973be563915d45f5364b1fd4e3
xxc <xxc>
2020-01-01 15:55:21 +0800

    初始化底部导航

---
+++ src/components/common/tabbar/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="main-tab-bar">
+    <tab-bar>
+      <tab-bar-item link="/home" activeColor="red">
+        <img slot="item-icon" src="~assets/imgs/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item link="/category" activeColor="red">
+        <img slot="item-icon" src="~assets/imgs/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item link="/cart" activeColor="red">
+        <img slot="item-icon" src="~assets/imgs/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/shopcart_active.svg" alt="">
+        <div slot="item-text">小车车</div>
+      </tab-bar-item>
+      <tab-bar-item link="/profile" activeColor="red">
+        <img slot="item-icon" src="~assets/imgs/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from 'components/common/tabbar/TabBar'
+  import TabBarItem from 'components/common/tabbar/TabBarItem'
+  
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>中</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Cart"
+  }
+</script>
+
+<style scoped>
+
+</style>

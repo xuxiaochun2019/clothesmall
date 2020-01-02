e2b97e678ec4684e2670bf650a04f0225cfa463d
xxc <xxc_pig@163.com>
2020-01-02 15:59:17 +0800

    封装axios实例 请求响应拦截器

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+// 封装request模块
+import {request} from './network/request'
+request({
+  url: '/home/multidata'
+}).then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
 
 new Vue({
   router,
